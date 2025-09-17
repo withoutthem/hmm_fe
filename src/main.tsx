e5c8fbd0d75e4ed7c57e4c521b190dd5e9f08d29
Hyejamomm <jokyounga1993@gmail.com>
2025-09-17 10:59:55 +0900

    publish/import수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import '@assets/fonts/pretendard/pretendard.css'
-import '@assets/fonts/notoSans/notoSansKR.css'
-import '@styles/reset.css'
-import '@styles/global.css'
-import AppErrorBoundary from '@app/providers/AppErrorBoundary'
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import '@assets/fonts/pretendard/pretendard.css';
+import '@styles/reset.css';
+import '@styles/global.css';
+import AppErrorBoundary from '@app/providers/AppErrorBoundary';
 
 createRoot(document.getElementById('root')!).render(
   <AppErrorBoundary>
     <App />
   </AppErrorBoundary>
-)
+);
